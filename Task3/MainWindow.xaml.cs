502cbef306405b613fc160e1dfed84418fd172e5
Egor Chugin <127592472+Gipeg@users.noreply.github.com>
2024-06-26 01:33:32 +0300

    Add files via upload

---
+++ Task3/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Task3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void GoodsCounter_ValueChanged(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(PriceTextBox.Text, out int cost))
+                CostLabel.Content = GoodsCounter.Value * cost;
+        }
+
+        private void PriceTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            CostLabel.Content = GoodsCounter.Value * Convert.ToInt32(PriceTextBox.Text);
+        }
+    }
+}
